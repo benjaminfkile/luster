142bc001175115e7795068386756bc633c4eb6cb
ben <benjaminfkile@gmail.com>
2020-08-27 01:36:42 -0600

    id wtf

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -54,7 +54,7 @@ class Post extends Component {
                     lng: Location.lng,
                     url: args,
                     rating: 5,
-                    id: uuid.v4(),
+                    id: Math.random()/Math.random,
                 })
             }).then((response) => {
             }).then(() => {
