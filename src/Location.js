d9fa064dcf6c97193004984e70108355d427e0bd
ben <benjaminfkile@gmail.com>
2020-09-09 22:13:29 -0600

    oops. test again

--- src/Location.js
+++ src/Location.js
@@ -1,16 +1,18 @@
 let Location = []
-let wait = setInterval(awaitUserAllow, 1000)
+let wait
 let wv = true
 
 function isWebview() {
     var ua = navigator.userAgent || navigator.vendor || window.opera;
-    if (!(ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1) || (ua.indexOf('Instagram') > -1));
-    wv = false
+    if (!(ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1) || (ua.indexOf('Instagram') > -1)){
+        getLocation()
+        wait = setInterval(awaitUserAllow, 1000)
+    }
 }
 
 function getLocation() {
 
-    if (!wv && navigator.geolocation) {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(setLocation)
     }
 }
@@ -21,8 +23,7 @@ function setLocation(position) {
 }
 
 function awaitUserAllow() {
-    // console.log("Waiting for user to allow location")
-    if (!wv && Location.lat) {
+    if (Location.lat) {
         clearInterval(wait)
         setInterval(function () {
             getLocation()
@@ -30,5 +31,4 @@ function awaitUserAllow() {
     }
 }
 isWebview()
-getLocation()
 export default Location
