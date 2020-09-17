282519a816a20d86762ee34c338310db9e00735f
ben <benjaminfkile@gmail.com>
2020-09-16 23:07:06 -0600

    start to a landing page for webview clients

--- src/InApp/InApp.js
+++ src/InApp/InApp.js
@@ -13,29 +13,31 @@ class InApp extends Component {
         }
     }
 
-    openInBrowser = () => {
-        window.open('https://luster.vercel.app/', '_system')
-    }
-
     render() {
         return (
             <div className="InApp">
                 <section id="notice">
-                    <h1>
-                        Welcome to Luster!
+                    <h1 id="welcome">
+                        Welcome to Luster
                 </h1>
                     <h2>
-                        You seem to be viewing this page inside of Facebook which is fine, if you want more features open it in your devices web browser
+                        You seem to be viewing this page inside of Facebook which is fine. If you want more features open it in your devices web browser
                 </h2>
-                    <h3 onClick={this.props.dismiss}>
+                    <h3 id="dismiss" onClick={this.props.dismiss}>
                         DISMISS
                 </h3>
-                    <h2>
-                        or..
+                <br></br>
+                <h2>
+                    or...
                 </h2>
-                <h3 onClick={this.openInBrowser}>
-                        OPEN IN BROWSER
+                    <h3 id="full-power">
+                        Want the full power of Luster?
                 </h3>
+                <br></br>
+                    <h2>
+                        Select the menu in the upper right hand corner and choose "Open with...".
+                </h2>
+                    <img src="https://i.ibb.co/w6yjKj1/1b311df9-c030-419c-849c-592456720d1e.png"></img>
                 </section>
                 {/* <Snow/> */}
             </div>
