f85eb33831ea33009ea9a46b2d3a8b302fe475ab
ben <benjaminfkile@gmail.com>
2020-09-18 23:00:37 -0600

    rework nag fb nag screen

--- src/InApp/InApp.js
+++ src/InApp/InApp.js
@@ -1,17 +1,7 @@
 import React, { Component } from 'react'
-import Snow from '../Snow/Snow'
 import '../InApp/InApp.css'
 
 class InApp extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            lightDex: -1,
-            db: false,
-            showFeed: true,
-        }
-    }
 
     render() {
         return (
@@ -23,23 +13,17 @@ class InApp extends Component {
                     <h2>
                         You seem to be viewing this page inside of Facebook which is fine. If you want more features open it in your devices web browser
                 </h2>
-                    <h3 id="dismiss" onClick={this.props.dismiss}>
-                        DISMISS
-                </h3>
-                <br></br>
-                <h2>
-                    or...
+                    <h2>
+                        or..
                 </h2>
-                    <h3 id="full-power">
-                        Want the full power of Luster?
-                </h3>
-                <br></br>
                     <h2>
                         Select the menu in the upper right hand corner and choose "Open with...".
                 </h2>
-                    <img src="https://i.ibb.co/w6yjKj1/1b311df9-c030-419c-849c-592456720d1e.png"></img>
+                    <img src="https://i.ibb.co/w6yjKj1/1b311df9-c030-419c-849c-592456720d1e.png" alt="oof"></img>
+                    <h2 id="dismiss" onClick={this.props.dismiss}>
+                        Dismiss
+                    </h2>
                 </section>
-                {/* <Snow/> */}
             </div>
         )
     }
