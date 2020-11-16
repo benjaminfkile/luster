90c4ac0bf13a2d84509afac2c051b2436c322338
ben <benjaminfkile@gmail.com>
2020-11-16 06:37:42 -0700

    fml^2

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -107,7 +107,7 @@ class Map extends React.Component {
   listenForQuery = () => {
     if (this.state.target && this.state.target !== Radar.targets[0][0] && this.mapMounted) {
       this.setState({ target: Radar.targets[0][0], lights: LightStore })
-      this.dragged()
+      // this.dragged()
       this.buildMarkers()
     }
   }
