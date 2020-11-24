a81474f5f23f8757cfc9b94586149671b0996593
ben <benjaminfkile@gmail.com>
2020-11-24 05:42:44 -0700

    try some more logic with toggling markers

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -89,7 +89,7 @@ class Map extends React.Component {
     if (LightStore.update.length > 0) {
       for (let i = 0; i < LightStore.update.length; i++) {
         if (LightStore.update[i] === 1) {
-          this.setState({ lights: LightStore.lights })
+          this.setState({ lights: LightStore.lights, lightDex: -1, search: false})
           this.recenter()
           this.buildMarkers()
         }
